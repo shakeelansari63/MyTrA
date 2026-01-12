d0342c24e15e1e3348c78810e451fab7bf8b3f23
Shakeel Ansari <ShakeelAnsari63@gmail.com>
2026-01-12 21:50:12 +0530

    Add tamagui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,17 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Stack } from "expo-router";
+import { TamaguiProvider, createTamagui } from "tamagui";
+import { defaultConfig } from "@tamagui/config/v4";
+import { themes } from "../theme";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+export const config = createTamagui({
+  ...defaultConfig,
+  themes,
+});
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
+export default function App() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <TamaguiProvider config={config}>
+      <Stack />
+    </TamaguiProvider>
   );
 }
